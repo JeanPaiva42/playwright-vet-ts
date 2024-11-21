6dcd585131bb525666672464d8ef8292e3fe0453
JeanPaiva42 <jean.miranda@toptal.com>
2024-11-21 16:51:55 +0100

    refactoring mainPage tests

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # playwright-vet-ts
 Small set of playwright tests to vetspire
+
+### Observations:
+
+- Some of these tests are very small, and some of them had their assertions moved to a single test case just so we could get closer to tests that follow (AAA pattern).
+- I am covering some of the mainPage, feature dropdown, pricing page and status page. I couldn't find bigger flows on the landing page, but I am sure that inside the app there's plenty of challenges. 
+- We also have a very simple github actions to run the tests that I created on this repository.
--- tests/e2e/mainPage.test.ts
+++ tests/e2e/mainPage.test.ts
@@ -10,36 +10,29 @@ var mainPage: MainPage;
 // I started doing them, and breaking into smaller cases, and turns out that some of them are just asserting, sadly I couldn't come back and fix it, but ideally they should
 
 test.describe('Verifications on the main page', async () => {
+
   test.beforeEach(async ({ page }) => {
     mainPage = new MainPage(page);
     await page.goto('https://vetspire.com/');
 
   });
 
-  test('confirms that "Feature" and "Schedule a Demo" button is visible on the main page', async ({ page }) => {
-    const linkName: string = 'Pricing';
+  test('confirms that some elements on the main page are being correctly loaded', async ({ page }) => {
+    const featuresLinkName: string = 'Features';
+    const pricingLinkName: string = 'Pricing';
+    const resourcesLinkName: string = 'Resources';
+    const demoButtonText: string = 'Schedule a Demo';
 
-    await expect(await mainPage.scheduleADemoButton()).toContainText('Schedule a Demo');
-    await expect(await mainPage.featuresLink()).toContainText('Features');
-
+    await expect(await mainPage.scheduleADemoButton()).toContainText(demoButtonText);
+    await expect(await mainPage.featuresLink()).toContainText(featuresLinkName);
+    await expect(await mainPage.princingLink()).toContainText(pricingLinkName);
+    await expect(await mainPage.resourcesLink()).toContainText(resourcesLinkName);
   });
-  test('confirms that "Pricing" button is visible on the main page', async ({ page }) => {
-    const linkName: string = 'Pricing';
-
-    await expect(await mainPage.princingLink()).toContainText(linkName);
-
-  });
-
-  test('confirms that "Resources" button is visible on the main page', async ({ page }) => {
-    const linkName: string = 'Resources';
-
-    await expect(await mainPage.resourcesLink()).toContainText(linkName);
-  })
 
   test('accesses the schedule a demo modal', async ({ page }) => {
-
     const scheduleADemoModal = new ScheduleADemoModal(page);
     const selectedPractice: string = 'Non-Profit';
+
     expect(await mainPage.scheduleADemoButton()).toBeVisible();
 
     await page.waitForLoadState('networkidle'); // Ensure all scripts are loaded
